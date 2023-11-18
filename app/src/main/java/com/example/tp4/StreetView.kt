b689f113b2575b60300b3705c02e0e1f0f70f5e9
nada <nadakhelif61@gmail.com>
2023-11-18 13:24:50 +0100

    final

---
+++ app/src/main/java/com/example/tp4/StreetView.kt
@@ -0,0 +1,40 @@
+package com.example.tp4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tp4.database.entities.Schedule
+import com.example.tp4.databinding.ActivityMainBinding
+import com.example.tp4.viewmodels.BusScheduleViewModel
+import com.example.tp4.viewmodels.BusScheduleViewModelFactory
+
+class StreetView : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: BusScheduleViewModel by viewModels() {
+        BusScheduleViewModelFactory((application as BusScheduleApplication).db.scheduleDae())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scheduleView.layoutManager = LinearLayoutManager(this)
+        binding.scheduleView.hasFixedSize()
+
+        var dataList = listOf<Schedule>()
+        val busStopAdapter = BusStopAdapter(dataList) { _ -> }
+
+        binding.scheduleView.adapter = busStopAdapter
+
+        val stopName = intent.getStringExtra("stopName")!!
+        viewModel.scheduleForStop(stopName).observe(this) {
+            busStopAdapter.updateList(it)
+        }
+
+    }
+}
